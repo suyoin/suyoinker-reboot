bad93572f99b4dcbe29cfdc7850a534a06eb7854
suyoin <19378617+suyoin@users.noreply.github.com>
2021-06-04 11:42:59 -0500

    debug

--- pages/api/interaction.ts
+++ pages/api/interaction.ts
@@ -17,19 +17,20 @@ commandFiles.forEach((file) => {
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+	console.log("a");
 	const parseBodyResult = await parseBody(req);
 	if (!parseBodyResult) {
 		res.status(400).end("[interaction]: Parse body failed");
 		return;
 	}
-
+	console.log("b");
 	const { parsedBody, rawBody } = parseBodyResult;
 	const interaction = parsedBody as APIInteraction;
 	if (!interaction.data) {
 		res.status(400).end("[interaction]: Interaction body is invalid");
 		return;
 	}
-
+	console.log("c");
 	const timestamp = (req.headers["x-signature-timestamp"] || "") as string;
 	const signature = (req.headers["x-signature-ed25519"] || "") as string;
 
@@ -37,17 +38,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		res.status(401).end("[interaction]: Invalid signature");
 		return;
 	}
-
+	console.log("d");
 	if (interaction.type === InteractionType.Ping) {
 		res.status(200).send({ type: InteractionResponseType.Pong });
 		return;
 	}
-
+	console.log("e");
 	if (!commands.has(interaction.data.name)) {
 		res.status(400).end("[interaction]: Command does not exist");
 		return;
 	}
-
+	console.log("f");
 	const commandResponse = await commands.get(interaction.data.name)!.execute(interaction);
 	res.status(200);
 	if (commandResponse) {
