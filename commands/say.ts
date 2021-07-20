dc3b27a83bc590678b4ce6f0276045d1f82cfdc3
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-20 09:29:37 -0500

    try to make anonymouse /say command

--- commands/say.ts
+++ commands/say.ts
@@ -1,10 +1,12 @@
+import axios from "axios";
 import {
 	ApplicationCommandInteractionDataOptionString,
 	InteractionResponseType,
 	InteractionType,
 } from "discord-api-types/v9";
+import { endpoint } from "../constant";
 
-export const execute = (interaction: LocalAPIInteraction) => {
+export const execute = async (interaction: LocalAPIInteraction) => {
 	if (interaction.type !== InteractionType.ApplicationCommand) {
 		return;
 	}
@@ -16,10 +18,27 @@ export const execute = (interaction: LocalAPIInteraction) => {
 	let text = messageOption.value;
 	text = text.replace("@everyone", "");
 
-	return {
-		type: InteractionResponseType.ChannelMessageWithSource,
+	await axios({
+		method: "POST",
+		url: `${endpoint}/interactions/${interaction.id}/${interaction.token}/callback`,
 		data: {
-			content: "",
+			type: InteractionResponseType.ChannelMessageWithSource,
+			data: {
+				content: "",
+			},
 		},
-	};
+	});
+
+	await axios({
+		method: "POST",
+		url: `${endpoint}/webhooks/${process.env.CLIENT_ID}/${interaction.token}`,
+		data: {
+			content: text,
+		},
+	});
+
+	await axios({
+		method: "DELETE",
+		url: `${endpoint}/webhooks/${process.env.CLIENT_ID}/${interaction.token}/messages/@original`,
+	});
 };
