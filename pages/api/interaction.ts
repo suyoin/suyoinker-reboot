35ab841d0f6225fb6241e2eaf408e44bb308b43a
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-20 00:58:25 -0500

    remove file suffix

--- pages/api/interaction.ts
+++ pages/api/interaction.ts
@@ -8,14 +8,12 @@ import verifyInteraction from "../../util/verifyInteraction";
 const { PUBLIC_KEY } = process.env;
 
 const commandFiles = readdirSync(path.join(process.cwd(), "commands")).filter((file) => {
-	console.log(`readdir ${file}`);
 	return !file.startsWith("commandDefinitions");
 });
 
 const commands = new Map<string, CommandExport>();
 commandFiles.forEach((file) => {
-	console.log(`add ${file}`);
-	commands.set(file, require(`../../commands/${file}`));
+	commands.set(file.replace(".ts", ""), require(`../../commands/${file}`));
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
