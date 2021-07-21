9a51d4b2196bae85e3f28b541eece90e11cdf4ef
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-20 23:21:11 -0500

    patch ApplicationCommandOptionType

--- commands/latex.ts
+++ commands/latex.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { InteractionResponseType, InteractionType } from "discord-api-types/v9";
+import { ApplicationCommandOptionType, InteractionResponseType, InteractionType } from "discord-api-types/v9";
 import { endpoint } from "../constant";
 import { createFormData } from "../util/createFormData";
 
@@ -10,51 +10,52 @@ export const execute = async (interaction: LocalAPIInteraction) => {
 
 	const latexInput = interaction.data.options!.find((obj) => {
 		return obj.name === "string";
-	});
-
-	const response = await axios({
-		method: "POST",
-		url: "http://latex2png.com/api/convert",
-		headers: {
-			"Content-type": "application/x-www-form-urlencoded",
-		},
-		data: {
-			auth: {
-				user: "guest",
-				password: "guest",
+	})!;
+	if (latexInput.type === ApplicationCommandOptionType.String) {
+		const response = await axios({
+			method: "POST",
+			url: "http://latex2png.com/api/convert",
+			headers: {
+				"Content-type": "application/x-www-form-urlencoded",
 			},
-			latex: latexInput,
-			resolution: 300,
-			color: interaction.data.options!.find((obj) => {
-				return obj.name === "black";
-			})
-				? "000000"
-				: "ffffff",
-		},
-	});
-
-	const imageResponse = await axios({
-		method: "GET",
-		url: `http://latex2png.com${response.data.url}`,
-		responseType: "stream",
-	});
-
-	await axios({
-		method: "POST",
-		url: `${endpoint}/interactions/${interaction.id}/${interaction.token}/callback`,
-		data: {
-			type: InteractionResponseType.ChannelMessageWithSource,
 			data: {
-				content: `Fetching latex for \`${latexInput}\`...`,
+				auth: {
+					user: "guest",
+					password: "guest",
+				},
+				latex: latexInput.value,
+				resolution: 300,
+				color: interaction.data.options!.find((obj) => {
+					return obj.name === "black";
+				})
+					? "000000"
+					: "ffffff",
 			},
-		},
-	});
+		});
 
-	const formData = createFormData(imageResponse.data);
-	await axios({
-		method: "POST",
-		url: `${endpoint}/webhooks/${process.env.CLIENT_ID}/${interaction.token}`,
-		headers: formData.getHeaders(),
-		data: formData,
-	});
+		const imageResponse = await axios({
+			method: "GET",
+			url: `http://latex2png.com${response.data.url}`,
+			responseType: "stream",
+		});
+
+		await axios({
+			method: "POST",
+			url: `${endpoint}/interactions/${interaction.id}/${interaction.token}/callback`,
+			data: {
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: {
+					content: `Fetching latex for \`${latexInput.value}\`...`,
+				},
+			},
+		});
+
+		const formData = createFormData(imageResponse.data);
+		await axios({
+			method: "POST",
+			url: `${endpoint}/webhooks/${process.env.CLIENT_ID}/${interaction.token}`,
+			headers: formData.getHeaders(),
+			data: formData,
+		});
+	}
 };
