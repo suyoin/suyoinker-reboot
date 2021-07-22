702bfdf3ee7361b09a01ea92fcda2b1186b9bcd2
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-22 00:35:36 -0500

    debug

--- commands/roles.ts
+++ commands/roles.ts
@@ -33,21 +33,25 @@ export const execute = async (interaction: APIMessageComponentInteraction) => {
 
 	const roles = await getDoc(doc(collection(db, "guilds"), interaction.guild_id!));
 	if (roles.exists()) {
+		console.log("exists");
 		return;
 	}
+	console.log("no exists");
 
 	const roleNameToId: { [key in keyof typeof roleColors]?: string } = {};
 	for (const colorName in roleColors) {
+		console.log("before create role");
 		roleNameToId[colorName as keyof typeof roleColors] = await createGuildRole(
 			interaction.guild_id!,
 			colorName,
 			roleColors[colorName as keyof typeof roleColors].colorValue,
 		);
+		console.log("create role");
 		pause(0.1);
 	}
-
+	console.log("before set doc ");
 	await setDoc(doc(collection(db, "guilds"), interaction.guild_id!), { roles: roleNameToId });
-
+	console.log("after set doc");
 	const menuOptions: APISelectMenuOption[] = Object.keys(roleColors).map((value) => {
 		const definition = roleColors[value as keyof typeof roleColors];
 		return {
