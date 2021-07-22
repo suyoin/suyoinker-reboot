9551540eb7170ea9761ccca89443d35e623c20f0
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-22 00:31:06 -0500

    return existing app if already initalized

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,19 @@
 export const getApp = async () => {
-	const { initializeApp } = await import("firebase/app");
+	const { initializeApp, getApps, getApp } = await import("firebase/app");
 
-	return initializeApp({
-		apiKey: "AIzaSyAw7TXKG-XDr3fZl23z5H3JnFaZTpfVgWY",
-		authDomain: "suyoinker.firebaseapp.com",
-		projectId: "suyoinker",
-		storageBucket: "suyoinker.appspot.com",
-		messagingSenderId: "1018821610467",
-		appId: "1:1018821610467:web:50fe7887c2b7a2c4f9aed6",
-		measurementId: "G-32W9ZJLKL2",
-	});
+	if (getApps().length < 1) {
+		return initializeApp({
+			apiKey: "AIzaSyAw7TXKG-XDr3fZl23z5H3JnFaZTpfVgWY",
+			authDomain: "suyoinker.firebaseapp.com",
+			projectId: "suyoinker",
+			storageBucket: "suyoinker.appspot.com",
+			messagingSenderId: "1018821610467",
+			appId: "1:1018821610467:web:50fe7887c2b7a2c4f9aed6",
+			measurementId: "G-32W9ZJLKL2",
+		});
+	} else {
+		return await getApp();
+	}
 };
 
 export const getDatabase = async (app: import("firebase/app").FirebaseApp) => {
