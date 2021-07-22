30aa6c3e1f3afffa5bd6c53bbcc4d339be109d4c
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-22 09:29:30 -0500

    fail if user already has the role

--- components/role_menu.ts
+++ components/role_menu.ts
@@ -1,14 +1,29 @@
 import axios from "axios";
 import { authorizationHeader, endpoint } from "../constant";
 import {
+	APIInteractionResponseChannelMessageWithSource,
 	APIInteractionResponseUpdateMessage,
 	APIMessageComponentInteraction,
 	APIMessageSelectMenuInteractionData,
 	InteractionResponseType,
+	MessageFlags,
 } from "../discord-api-types/v9";
 
 export const execute = async (interaction: APIMessageComponentInteraction) => {
 	const roleId = (interaction.data as APIMessageSelectMenuInteractionData).values[0];
+
+	//already has the role
+	if (interaction.member!.roles.indexOf(roleId as `${bigint}`) !== -1) {
+		const response: APIInteractionResponseChannelMessageWithSource = {
+			type: InteractionResponseType.ChannelMessageWithSource,
+			data: {
+				content: "You already have this role!",
+				flags: MessageFlags.Ephemeral,
+			},
+		};
+
+		return response;
+	}
 
 	await axios({
 		method: "PUT",
