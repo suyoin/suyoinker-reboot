20bf867e6e48a6922b841256c19c22e34bc9a5e3
suyoin <19378617+suyoin@users.noreply.github.com>
2021-07-22 23:29:32 -0500

    new colors

--- data/roleColors.ts
+++ data/roleColors.ts
@@ -15,6 +15,13 @@ export interface ColorDefinition {
 }
 
 export default TypedObjectLiteral<ColorDefinition>()({
-	purple: { emojiId: "865778039928651818", colorValue: 10181046, animated: true },
+	aqua: { emojiId: "865778039928651818", colorValue: 2, animated: true },
+	green: { emojiId: "865778039928651818", colorValue: 3066993, animated: true },
 	blue: { emojiId: "865778039928651818", colorValue: 3447003, animated: true },
+	purple: { emojiId: "865778039928651818", colorValue: 10181046, animated: true },
+	gold: { emojiId: "865778039928651818", colorValue: 15844367, animated: true },
+	orange: { emojiId: "865778039928651818", colorValue: 15105570, animated: true },
+	red: { emojiId: "865778039928651818", colorValue: 15158332, animated: true },
+	navy: { emojiId: "865778039928651818", colorValue: 3426654, animated: true },
+	yellow: { emojiId: "865778039928651818", colorValue: 16776960, animated: true },
 });
